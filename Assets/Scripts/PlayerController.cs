2d7edb904986c390e781cab46ef702f830db3c2a
Jehdi Aizon <jaizon2000@gmail.com>
2020-12-23 19:41:22 -0700

    Make vehicle rotate instead of slide

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,21 +8,21 @@ public class PlayerController : MonoBehaviour
     public float turnSpeed;
 
     public float horizontalInput;
-    
+    public float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        
+        forwardInput = Input.GetAxis("Vertical");
+
         // Move vehicle forward
-        // Moving turnSpeed meter/sec
-        transform.Translate(Vector3.forward * (Time.deltaTime * speed));
-        transform.Translate(Vector3.right * (Time.deltaTime * turnSpeed * horizontalInput));
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed * forwardInput));
+        transform.Rotate(Vector3.up * (Time.deltaTime * turnSpeed * horizontalInput));
     }
 }
