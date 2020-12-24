a907ac409d633e6d2a58b49955756f2b54bd8beb
Jehdi Aizon <jaizon2000@gmail.com>
2020-12-23 18:47:20 -0700

    Initial Commit

---
+++ Assets/Scripts/FollowScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 initialPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPos = transform.position;
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+        // Camera follows vehicle
+        // transform.position = player.transform.position + new Vector3(0, 5, -11);
+        transform.position = player.transform.position + initialPos;
+    }
+}
