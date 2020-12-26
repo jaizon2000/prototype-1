33c3c2d2ffa62926d6c853744bc786587c35de25
Jehdi Aizon <jaizon2000@gmail.com>
2020-12-25 22:40:19 -0700

    rotate plane up/down

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -6,6 +6,7 @@ public class PlayerControllerX : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
+    
     public float verticalInput;
 
     // Start is called before the first frame update
@@ -15,15 +16,16 @@ public class PlayerControllerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
+        
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed));
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * (Time.deltaTime * rotationSpeed * verticalInput * -1));
     }
 }
